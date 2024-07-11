c4358b2ee8d3ceb79c868528e35a0ebc112be973
Judith Kurian <judith.kurian@foodhub.com>
2024-07-10 21:12:49 -0300

    check

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ def sum_product():
     data = request.json
     file_name = data['file']
     product = data['product']
+    print("entered service 2")
     
     # Construct the file path relative to current directory
-    file_path = os.path.join('/judith_PV_dir', file_name)
+    file_path = os.path.join('/judith_PV_dir/', file_name)
     # Check if the file exists
     if not os.path.exists(file_path):
         return jsonify(OrderedDict([("error", "File not found."), ("file", file_name)]))
@@ -56,3 +57,37 @@ def sum_product():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=7000)
+
+# import csv
+
+# #app2 is used to calculate the price of the product
+
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route("/sum", methods=["POST"])
+# def sum():
+#     sum = 0
+
+#     # 1. Calculate sum of the product amounts
+#     with open("/judith_PV_dir/"+request.json["file"]) as csvfile:
+#         csv_reader = csv.reader(csvfile, delimiter=',')
+#         for row in csv_reader:
+#             if len(row) != 2:
+#                 return { 
+#                         "file": request.json["file"], 
+#                         "error": "Input file not in CSV format." 
+#                        }
+#             if(row[0] == request.json["product"]):
+#                 sum = sum + int(row[1])
+
+#     # 2. return the sum along with the file name.
+#     return { 
+#             "file": request.json["file"], 
+#             "sum": sum
+#         }
+
+# if _name_ == "_main_":
+#     app.json.sort_keys = False
+#     app.run(host="0.0.0.0", port=7000)
